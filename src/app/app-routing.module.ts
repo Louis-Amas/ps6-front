93ef983d924cc573a019712f3493234f8d459507
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-03-19 11:46:23 +0100

    add root management #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StudentHomeComponent} from './student-home/student-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'student', component: StudentHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
