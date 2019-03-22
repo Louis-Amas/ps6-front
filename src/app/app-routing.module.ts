36c40b3ceb61040a9516ff3759b80854918fc220
AlexiaGIRARD <alexia.girard@etu.unice.fr>
2019-03-22 09:40:49 +0100

    Added router link to connection-page and update css #4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {StudentHomeComponent} from './student-home/student-home.component';
 import {HomeComponent} from './home/home.component';
+import { ConnectionComponent} from './connection/connection.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'student', component: StudentHomeComponent}
+  {path: 'student', component: ConnectionComponent},
+  {path: 'teacher', component: ConnectionComponent},
+  {path: 'bri', component: ConnectionComponent},
 ];
 
 @NgModule({
