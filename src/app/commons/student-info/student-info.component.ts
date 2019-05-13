b7943bc78b5fb0607ecdc36b064541982bf34c2c
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-05-13 21:56:49 +0200

    add back-header missed and fix some issues #45

--- src/app/commons/student-info/student-info.component.ts
+++ src/app/commons/student-info/student-info.component.ts
@@ -8,37 +8,6 @@ import {User} from '../../../models/user';
 })
 export class StudentInfoComponent implements OnInit {
 
-  public MAJOR_LIST: any[] = [
-    {
-      major: 'BAT',
-      specialty: []
-    },
-    {
-      major: 'SI',
-      specialty: ['Architecture logicielle', 'Cryptographie, sécurité et vie privée dans les applications et les réseaux',
-        'Intelligence Ambiante', 'Interactions Homme - Machine', 'Sciences, Technologies, Ressources et Applications du Web',
-        'Informatique et Mathématiques Appliquées à la Finance et à l\'Assurance', 'Science des Données']
-    },
-    {
-      major: 'MAM',
-      specialty: ['Informatique et mathématiques appliquées à la finance et l\'assurance', 'Ingénierie numérique',
-        'Science des données']
-    },
-    {
-      major: 'ELEC',
-      specialty: ['Conception de circuits et systèmes', 'Génie du système embarqué', 'Télécommunications et réseaux ']
-    },
-    {
-      major: 'GB',
-      specialty: ['Pharmacologie et Biotechnologie', 'Toxicologie et Sécurité en Santé et Environnement',
-        'Bioinformatique et Modélisation pour la Biologie']
-    },
-    {
-      major: 'GE',
-      specialty: ['Exploitation des services publics de l\'eau', 'Hydroinformatique']
-    },
-  ];
-
   @Input() currentStudent: User;
 
   constructor() { }
